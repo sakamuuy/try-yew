a0063cf5780414fd1b16c4f5f047170a0257b1ad
sakamuuy <sakamuu.y@gmail.com>
2022-12-07 22:26:11 +0900

    render todo list

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use yew::prelude::*;
 use components::header::Header;
+use components::todo::todo_list::TodoList;
 
 mod components;
 
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <Header />
+        <>
+            <Header />
+            <main class="container-fluid mt-2">
+                <TodoList />
+            </main>
+        </>
     }
 }
 
