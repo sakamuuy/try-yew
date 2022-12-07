ec2c868a55e2224a96dd486aa01c2152fee8be60
sakamuuy <sakamuu.y@gmail.com>
2022-12-07 23:04:11 +0900

    interactive todo list

--- src/components/todo/todo_list.rs
+++ src/components/todo/todo_list.rs
@@ -1,29 +1,17 @@
 use crate::components::todo::todo_item::TodoItem;
 use crate::components::todo::types::Todo;
-use yew::{function_component, html, Html};
+use yew::{function_component, html, Html, Properties};
+
+#[derive(Properties, PartialEq)]
+pub struct TodoListProps {
+  pub todo_items: Vec<Todo>
+}
 
 #[function_component(TodoList)]
-pub fn todo_list() -> Html {
-    let todo_items = vec![
-        Todo {
-            id: 1,
-            title: "Learn Rust".to_string(),
-            completed: false,
-        },
-        Todo {
-            id: 2,
-            title: "Learn Yew".to_string(),
-            completed: true,
-        },
-        Todo {
-            id: 3,
-            title: "Build editor".to_string(),
-            completed: false,
-        },
-    ];
+pub fn todo_list(props: &TodoListProps) -> Html {
     html! {
       <ul class="list-group">
-        {todo_items.iter().map(|todo| html! {
+        {props.todo_items.iter().map(|todo| html! {
           <TodoItem title={todo.title.clone()} completed={todo.completed} />
         }).collect::<Html>()}
       </ul>
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use components::header::Header;
+use components::todo::types::Todo;
 use components::todo::todo_form::TodoForm;
 use components::todo::todo_list::TodoList;
 use yew::prelude::*;
@@ -7,14 +8,29 @@ mod components;
 
 #[function_component(App)]
 fn app() -> Html {
-    let on_add = { Callback::from(move |title: String| log::info!("on_add: {:?}", title)) };
+    let todo_items = use_state(|| Vec::<Todo>::new());
+    let next_id = use_state(||1);
+
+    let on_add = { 
+        let todo_items = todo_items.clone();
+        Callback::from(move |title: String| {
+            let mut current_todo_items = (*todo_items).clone();
+            current_todo_items.push(Todo {
+                id: *next_id,
+                title,
+                completed: false
+            });
+            next_id.set(*next_id + 1);
+            todo_items.set(current_todo_items);
+        }) 
+    };
 
     html! {
         <>
             <Header />
             <main class="container-fluid mt-2">
                 <TodoForm {on_add} />
-                <TodoList />
+                <TodoList todo_items={(*todo_items).clone()} />
             </main>
         </>
     }
